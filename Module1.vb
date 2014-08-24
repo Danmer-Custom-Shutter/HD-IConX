fff5b82d780fed7598c031fc049f2c8d6937374a
bradmkjr <brad@ucanbonline.net>
2014-08-24 12:58:40 -0600

    initial commit with functions

--- Module1.vb
+++ Module1.vb
@@ -1,7 +1,56 @@
-﻿Module Module1
+﻿Imports System
+Imports System.IO
+Imports System.Net
+Imports System.Text
+Imports Newtonsoft.Json
+
+Module Module1
 
     Sub Main()
 
+
+        Dim app_token As String = FetchAppToken()
+
+        Console.Write(app_token)
+
+
     End Sub
+
+    Public Function FetchAppToken()
+        ' removed client_id, add back in to make functional
+        ' removed client_secret, add back in to make functional
+        Dim postData As String = "client_id=HlQt96tynmwYDYmatN4EyMtG23imNoQa&client_secret=5LAFGbnbEpmsABMD"
+        Dim tempCookies As New CookieContainer
+        Dim encoding As New UTF8Encoding
+        Dim byteData As Byte() = encoding.GetBytes(postData)
+
+        Dim postReq As HttpWebRequest = DirectCast(WebRequest.Create("https://api.hs.homedepot.com/iconx/v1/auth/accesstoken?grant_type=client_credentials"), HttpWebRequest)
+        postReq.Method = "POST"
+        ' postReq.KeepAlive = True
+        ' postReq.CookieContainer = tempCookies
+        postReq.ContentType = "application/x-www-form-urlencoded"
+        postReq.Referer = "http://www.danmer.com/"
+        postReq.UserAgent = "Mozilla/5.0 (Windows; U; Windows NT 6.1; ru; rv:1.9.2.3) Gecko/20100401 Firefox/4.0 (.NET CLR 3.5.30729)"
+        postReq.ContentLength = byteData.Length
+
+        Dim postreqstream As Stream = postReq.GetRequestStream()
+        postreqstream.Write(byteData, 0, byteData.Length)
+        postreqstream.Close()
+        Dim postresponse As HttpWebResponse
+
+        postresponse = DirectCast(postReq.GetResponse(), HttpWebResponse)
+        ' tempCookies.Add(postresponse.Cookies)
+        ' logincookie = tempCookies
+        Dim postreqreader As New StreamReader(postresponse.GetResponseStream())
+
+        Dim thepage As String = postreqreader.ReadToEnd
+
+        Dim decodedresponse = JsonConvert.DeserializeObject(Of client_credentials)(thepage)
+
+        ' Console.WriteLine(decodedresponse.access_token)
+
+        Return decodedresponse.access_token
+
+    End Function
 
 End Module
